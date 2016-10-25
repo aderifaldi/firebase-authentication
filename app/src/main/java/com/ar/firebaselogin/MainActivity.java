dac5f44f8015977fc0aed961184f7be3daaf2a1d
aderifaldi <ade.rifaldi@gmail.com>
2016-10-25 22:36:14 +0700

    add project

---
+++ app/src/main/java/com/ar/firebaselogin/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ar.firebaselogin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.emailPassword)
+    protected void emailPassword(){
+        startActivity(new Intent(getApplicationContext(), EmailPasswordActivity.class));
+    }
+
+}
