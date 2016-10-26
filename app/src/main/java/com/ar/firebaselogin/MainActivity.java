bbdb04232ebfa7058ae75deb6c594bc3ca075ce9
aderifaldi <ade.rifaldi@gmail.com>
2016-10-26 07:06:31 +0700

    add facebook login

--- app/src/main/java/com/ar/firebaselogin/MainActivity.java
+++ app/src/main/java/com/ar/firebaselogin/MainActivity.java
@@ -1,8 +1,16 @@
 package com.ar.firebaselogin;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -14,11 +22,35 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        printKeyHash();
     }
 
     @OnClick(R.id.emailPassword)
     protected void emailPassword(){
         startActivity(new Intent(getApplicationContext(), EmailPasswordActivity.class));
+    }
+
+    @OnClick(R.id.facebookLogin)
+    protected void facebookLogin(){
+        startActivity(new Intent(getApplicationContext(), FacebookLoginActivity.class));
+    }
+
+    private void printKeyHash(){
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.ar.firebaselogin",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", "hash key = " + Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d("KeyHash:", e.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.d("KeyHash:", e.toString());
+        }
     }
 
 }
